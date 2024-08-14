be9f40ebb970e7760b9c2187feeb8152acbd5c84
Deepak Vishwakarma <18vdeepak@gmail.com>
2024-08-14 21:24:14 +0530

    commit

---
+++ tailwind-react/src/bgChanger/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'bgChanger/node_modules/@types/react'
+import reactLogo from 'bgChanger/src/assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bgChanger/src/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
